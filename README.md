7856eee753191bb5228b94dde34351236e3dd208
Alexandre <xandykati98@gmail.com>
2021-02-09 16:47:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# sml-recommender-sdk
+ Kit de desenvolvimento feito para facilitar o processo de gerar recomendações de imóveis para leads.
+
+### Funcionalidades
+
+- [ ] Recomendação de imóveis por leads
+    - [ ] Imóveis semelhantes baseado em descrições usando TF-IDF
+    - [ ] Baseado em características dos imóveis
---
+++ src/cos_similarity.ts
@@ -0,0 +1,21 @@
+import dot_product from "./dot_product";
+
+/**
+ * @description Cosine similarity is a measure of similarity between two non-zero vectors of an inner product space.
+ * @see https://towardsdatascience.com/how-to-build-a-textual-similarity-analysis-web-app-aa3139d4fb71#8762
+ * @param {number[]} a Vetor unidimensional de números
+ * @param {number[]} b Vetor unidimensional de números
+ */
+const cos_similarity = (a:number[], b:number[]) => {
+    const magnitude_a = Math.sqrt(dot_product(a, a));
+    const magnitude_b = Math.sqrt(dot_product(b, b));
+    
+    if (magnitude_a && magnitude_b)
+        /**
+         * @see https://wikimedia.org/api/rest_v1/media/math/render/svg/1d94e5903f7936d3c131e040ef2c51b473dd071d
+         */
+        return dot_product(a, b) / (magnitude_a * magnitude_b);
+    else return false
+}
+
+export default cos_similarity
